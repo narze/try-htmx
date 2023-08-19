09fe50f7937dbc797d4657242bc3b7900343760d
Manassarn Manoonchai <manassarn@gmail.com>
2023-08-20 00:42:20 +0700

    Try fixing serverless.js

--- api/serverless.js
+++ api/serverless.js
@@ -1,11 +1,8 @@
 "use strict"
 
-// Read the .env file.
-import * as dotenv from "dotenv"
-dotenv.config()
-
 // Require the framework
-import Fastify from "fastify"
+const Fastify = require("fastify")
+const view = require("@fastify/view")
 
 // Instantiate Fastify with some config
 const app = Fastify({
@@ -22,7 +19,7 @@ app.register(require("@fastify/formbody"))
 // Register your application as a normal plugin.
 app.register(require("../app.js"))
 
-export default async (req, res) => {
+module.exports = async (req, res) => {
   await app.ready()
   app.server.emit("request", req, res)
 }
